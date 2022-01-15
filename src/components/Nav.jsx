dfa1c972cf371face5b507a59123dd34225cf0fb
RizwanAmjad <rizwanamjadms@gmail.com>
2022-01-15 10:43:44 +0500

    Improved Navigation style and added toggle button to show and hide navigation labels

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { NavLink } from "react-router-dom";
+
+import {
+  MdHome,
+  MdCategory,
+  MdPalette,
+  MdToggleOff,
+  MdToggleOn,
+} from "react-icons/md";
 
 import "./styles/nav.css";
 
 function Nav(props) {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <ul className="nav">
+    <ul className={`nav ${!expanded ? "nav-collapsed" : ""}`}>
+      <span id="expand-button" onClick={() => setExpanded(!expanded)}>
+        {expanded ? <MdToggleOn /> : <MdToggleOff />}
+      </span>
       <NavLink to="/">
-        <li className="nav-item">Home</li>
+        <li className="nav-item">
+          <MdHome />
+          <div className="nav-title">Home</div>
+        </li>
       </NavLink>
       <NavLink to="/categories">
-        <li className="nav-item">Categories</li>
+        <li className="nav-item">
+          <MdCategory />
+          <div className="nav-title">Categories</div>
+        </li>
       </NavLink>
       <NavLink to="/paintings">
-        <li className="nav-item">Paintings</li>
+        <li className="nav-item">
+          <MdPalette />
+          <div className="nav-title">Paintings</div>
+        </li>
       </NavLink>
     </ul>
   );
